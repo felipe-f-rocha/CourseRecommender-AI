86ec82c2da06c7ae390d248749d0fd46e53d7f7d
Felipe Rocha <felipefernandescruzeiro2017@gmail.com>
2026-07-01 17:38:27 -0300

    create error handler and improved input validation

---
+++ services/input_validator.py
@@ -0,0 +1,13 @@
+from domain.exceptions import InvalidInput
+from domain import input_validations
+
+def validate(area):
+
+    try:
+        area = input_validations.is_empty(area)
+        area = input_validations.length(area)
+        area = input_validations.check_number(area)
+    except InvalidInput:
+        raise 'Invalid Input'
+    else:
+        return area
